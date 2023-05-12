095b193539197b77c03a129229354fd521aca0b6
Богдан Иджилов <idbogdan2004@gmail.com>
2023-05-12 12:08:46 +0300

    feature: added server-client part

---
+++ src/Client.java
@@ -0,0 +1,51 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Client {
+    public static void main(String arg[]) throws IOException {
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+
+        Socket socket = null;
+
+        try {
+            socket = new Socket("localhost", 1234);
+            System.out.println("Connected to server");
+
+            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+
+            bufferedReader = new BufferedReader(inputStreamReader);
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                System.out.print("Client: ");
+                String msgToServer = scanner.nextLine();
+
+                bufferedWriter.write(msgToServer);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+
+                String msgFromServer = bufferedReader.readLine();
+                System.out.println("Server: " + msgFromServer);
+
+                if(msgToServer.equalsIgnoreCase("bye")) break;
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null)
+                bufferedReader.close();
+            if (bufferedWriter != null)
+                bufferedWriter.close();
+            if (socket != null)
+                socket.close();
+        }
+    }
+}
